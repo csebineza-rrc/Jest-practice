27116e4d2125d144f95bc331189554b0e6097762
csebineza-rrc <csebineza@academic.rrc.ca>
2025-04-25 00:17:03 -0500

    Implemented new functions with unit tests.

---
+++ math.test.js
@@ -0,0 +1,23 @@
+// math.test.js
+
+const { add, subtract, multiply, divide } = require('./math');
+
+test('adds 2 + 3 to equal 5', () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test('subtracts 5 - 2 to equal 3', () => {
+  expect(subtract(5, 2)).toBe(3);
+});
+
+test('multiplies 4 * 3 to equal 12', () => {
+  expect(multiply(4, 3)).toBe(12);
+});
+
+test('divides 10 / 2 to equal 5', () => {
+  expect(divide(10, 2)).toBe(5);
+});
+
+test('dividing by zero throws an error', () => {
+  expect(() => divide(10, 0)).toThrow("Cannot divide by zero");
+});
